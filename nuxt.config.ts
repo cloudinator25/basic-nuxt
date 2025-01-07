921f03067ba89b2908b7d93d130dc2a830fec820
Vathanak <102912156+VathanakSol@users.noreply.github.com>
2025-01-07 21:05:43 +0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  // Nitro configuration for standalone output
-  nitro: {
-    preset: 'node-server', // Use 'node-server' for Node.js deployment
-    output: {
-      dir: '.output', // Output directory (default is '.output')
-      standalone: true, // Enable standalone output
-    },
-  },
+  output: {
+    standalone: true,
+  }
 })
